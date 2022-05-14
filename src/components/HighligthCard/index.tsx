0eac8b3de9c784e3778fa8cb594bc2d48b37ca4e
luiscostalafc <Nanico2406>
2022-05-14 15:10:19 -0300

    feat: create HighlightCard component

--- src/components/HighligthCard/index.tsx
+++ src/components/HighligthCard/index.tsx
@@ -10,17 +10,35 @@ import {
   LastTransaction,
 } from "./styles";
 
-export const HighligthCard = () => {
+interface HigligthCardProps {
+  title: string;
+  amount: string;
+  lastTrasaciton: string;
+  type: "up" | "down" | "total";
+}
+
+const icon = {
+  up: "arrow-up-circle",
+  down: "arrow-down-circle",
+  total: "dollar-sign",
+};
+
+export const HighligthCard = ({
+  type,
+  title,
+  amount,
+  lastTrasaciton,
+}: HigligthCardProps) => {
   return (
-    <Container>
+    <Container type={type}>
       <Header>
-        <Title>Entrada</Title>
-        <Icon name="arrow-up-circle" />
+        <Title type={type}>{title}</Title>
+        <Icon name={icon[type]} type={type} />
       </Header>
 
       <Footer>
-        <Amount>R$ 17.400,00</Amount>
-        <LastTransaction>Última entrada dia 13 de abril</LastTransaction>
+        <Amount type={type}>{amount}</Amount>
+        <LastTransaction type={type}>{lastTrasaciton}</LastTransaction>
       </Footer>
     </Container>
   );
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -22,9 +22,24 @@ export function Dashboard() {
         </S.UserWrapper>
       </S.Header>
       <S.HighligthCards>
-        <HighligthCard />
-        <HighligthCard />
-        <HighligthCard />
+        <HighligthCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTrasaciton="Última entrada dia 13 de abril"
+        />
+        <HighligthCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTrasaciton="Última saída dia 03 de abril"
+        />
+        <HighligthCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTrasaciton="01 à 16 de abril"
+        />
       </S.HighligthCards>
     </S.Container>
   );
